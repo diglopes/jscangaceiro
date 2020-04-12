0a9f22be0d6d12ad054dbef3c7e015d9a2ab5327
Diego Lopes <diego-chez@hotmail.com>
2020-04-12 11:49:23 -0300

    Move negotiation script to its domain folder

---
+++ client/app/domain/negotiation/Negotiation.js
@@ -0,0 +1,24 @@
+class Negotiation {
+  constructor(date, quantity, value) {
+    this._date = new Date(date.getTime());
+    this._quantity = quantity;
+    this._value = value;
+    Object.freeze(this);
+  }
+
+  get date() {
+    return new Date(this._date.getTime());
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get Volume() {
+    return this._quantity * this._value;
+  }
+}
