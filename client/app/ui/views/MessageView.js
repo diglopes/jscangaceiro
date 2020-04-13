7e4093225f9f4d26759bfd88631eba09c70badac
Diego Lopes <diego-chez@hotmail.com>
2020-04-13 15:17:22 -0300

    Throw an error if template isn't implemented

--- client/app/ui/views/MessageView.js
+++ client/app/ui/views/MessageView.js
@@ -1,7 +1,7 @@
 class MessageView extends View {
   template(model) {
     return `
-      <p class="alert alert-success">${model.text}</p>
-      `;
+        <p class="alert alert-success">${model.text}</p>
+        `;
   }
 }
--- client/app/ui/views/View.js
+++ client/app/ui/views/View.js
@@ -3,6 +3,10 @@ class View {
     this._element = document.querySelector(selector);
   }
 
+  template(model) {
+    throw new Error("Method template was not implemented yet.");
+  }
+
   update(model) {
     this._element.innerHTML = this.template(model);
   }
