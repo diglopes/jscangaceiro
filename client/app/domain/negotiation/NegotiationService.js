b09684c1db23743feea74b24166b5498facdb520
Diego Lopes <diego-chez@hotmail.com>
2020-04-15 17:33:02 -0300

    Remove explicit return on then

--- client/app/domain/negotiation/NegotiationService.js
+++ client/app/domain/negotiation/NegotiationService.js
@@ -5,13 +5,11 @@ class NegotiationService {
 
   getTheWeekNegotiations() {
     return this._http.get("negociacoes/semana").then(
-      (data) => {
-        const negotiations = data.map(
+      (data) =>
+        data.map(
           ({ data, quantidade, valor }) =>
             new Negotiation(new Date(data), quantidade, valor)
-        );
-        return negotiations;
-      },
+        ),
       (err) => {
         throw new Error("Não foi possivel obter as negociações.");
       }
@@ -20,13 +18,11 @@ class NegotiationService {
 
   getLastWeekNegotiations() {
     return this._http.get("negociacoes/anterior").then(
-      (data) => {
-        const negotiations = data.map(
+      (data) =>
+        data.map(
           ({ data, quantidade, valor }) =>
             new Negotiation(new Date(data), quantidade, valor)
-        );
-        return negotiations;
-      },
+        ),
       (err) => {
         throw new Error(
           "Não foi possivel obter as negociações da semana anterior."
@@ -37,13 +33,11 @@ class NegotiationService {
 
   getOldestNegotiations() {
     return this._http.get("negociacoes/retrasada").then(
-      (data) => {
-        const negotiations = data.map(
+      (data) =>
+        data.map(
           ({ data, quantidade, valor }) =>
             new Negotiation(new Date(data), quantidade, valor)
-        );
-        return negotiations;
-      },
+        ),
       (err) => {
         throw new Error(
           "Não foi possivel obter as negociações da semana retrasada."
