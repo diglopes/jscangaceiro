073412ad26af0906be400e2f78b5b04587a71e33
Diego Lopes <diego-chez@hotmail.com>
2020-04-23 09:01:48 -0300

    Add a post request to test server route

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,18 @@
 import { NegotiationController } from "./controllers/NegotiationController.js";
+import { Negotiation } from "./domain/index.js";
+
+// Testing the POST route
+const negotiation = new Negotiation(new Date(), 1, 200);
+const headers = new Headers();
+headers.set("Content-type", "Application/json");
+const config = {
+  method: "POST",
+  headers,
+  body: JSON.stringify(negotiation),
+};
+fetch("/negociacoes", config).then(() =>
+  console.log("Dado enviado com sucesso!")
+);
 
 const $ = document.querySelector.bind(document);
 
